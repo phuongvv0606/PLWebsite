91923011d1a15d60906d80dbb4ef6957b3a0e544
phuongvv0606 <phuongvv.chevrolet@gmail.com>
2017-07-03 09:08:24 +0700

    sdfdsf
    wd fsdfsd

---
+++ WebApplication19/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication19.Models;
+
+namespace WebApplication19.Controllers
+{
+    public class HomeController : Controller
+    {
+        plEntities db = new plEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetSlider()
+        {
+            return PartialView();
+        }
+        public ActionResult GetMenu()
+        {            
+            return PartialView(db.Menus.OrderBy(m => m.Pioriti).ToList());
+        }
+    }
+}
